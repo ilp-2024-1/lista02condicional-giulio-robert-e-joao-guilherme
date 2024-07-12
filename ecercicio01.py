f706bdb8dc2c7297e5a148bc85e33f89e708299d
RobertMN076 <robert.giulio@escolar.ifrn.edu.br>
2024-07-12 10:13:53 -0300

    questã 01, 03 e 05

---
+++ ecercicio01.py
@@ -0,0 +1,63 @@
+#Questão 01
+
+# salario = float(input('Declare seu salário:'))
+# isento = float(1903.98)
+# porcentagem1 = 7.5 / 100
+# porcentagem2 = 15 / 100
+# porcentagem3 = 22 / 100
+# porcentagem4 = 27.5 / 100
+
+# if salario <= isento:
+#     print('Você está isento do imposto de renda!')
+# elif salario <= 2826.65:
+#     imposto = salario * porcentagem1
+#     print(f'O seu imposto é de {imposto}')
+# elif salario <= 3751.05:
+#     imposto = salario * porcentagem2
+#     print(f'O seu imposto é de {imposto}')
+# elif salario <= 4664.68:
+#     imposto = salario * porcentagem3
+#     print(f'O seu imposto é de {imposto}')
+# else:
+#     imposto = salario * porcentagem4
+#     print(f'O seu imposto é de {imposto}')
+# print('Cuidado com o leão...')
+
+#Questão 03
+
+# numero_solicitado = int(input('Digite um número entre 1 e 10:'))
+
+# if numero_solicitado >= 1 and numero_solicitado <= 10:
+#     print('O número digitado está DENTRO da faixa solicitada')
+# else:
+#     print('O número digitado está FORA da faixa solicitada')
+
+#Questão 05
+
+import random
+
+# numeroaleatorio1 = random.randint(1, 500)
+# numeroaleatorio2 = random.randint(1, 500)
+
+# if numeroaleatorio1 > numeroaleatorio2:
+#     diferença = numeroaleatorio1 - numeroaleatorio2
+#     print(f'A diferença do primeiro número para o segundo é:{diferença}')
+# elif numeroaleatorio2 > numeroaleatorio1:
+#     diferença = numeroaleatorio2 - numeroaleatorio1
+#     print(f'A diferença do segundo número para o primeiro é:{diferença}')
+# else:
+#     print('Os números são iguais')
+
+#Questão 07
+
+numero_solicitado = int(input('Digite um número:'))
+numero_solicitado2 = int(input('Digite um número:'))
+numero_solicitado3 = int(input('Digite um número:'))
+
+
+desejo = input('Deseja ver os valores em ordem crescente ou descrescente?')
+
+
+
+
+
